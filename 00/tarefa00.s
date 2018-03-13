cd3701addad7827f1343387110a893d1b863042c
Giovanni Bertao <ra173325@gambit.lab.ic.unicamp.br>
2018-03-13 02:28:38 -0300

    Lab feito

---
+++ 00/tarefa00.s
@@ -0,0 +1,20 @@
+@ MC 404 - Lab 00 - Giovanni Bertão - ra173325
+@ Reservar espaço para 3 variáveis inteiras,
+@ Somar os valores das 3, somar o resultado a uma constante e
+@ armazenar o total em r0.
+
+		.org 100		@ Definindo inicio do bloco de variaveis
+varA:	.skip 4			@ VarA inicia em 0x100
+varB:	.skip 4			@ VarB inicia em 0x104
+varC:	.skip 4			@ VarC inicia em 0x108
+
+		.org 200		@ Definindo inicio do bloco de instruções
+start:
+		ld r0,varA		@ Somando o valor das 3 variaveis junto
+		ld r1,varB		@ da constante e armazenando os valores
+		add r0,r1		@ em r0
+		ld r1,varC
+		add r0,r1
+		set r1,0x1000
+		add r0,r1
+		hlt				@ Para
