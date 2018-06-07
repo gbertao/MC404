23a66778bb689ad4f89417d9e418b88de3a68db1
Giovanni <gbertao96@gmail.com>
2018-06-07 00:38:21 -0300

    Feito e submetido

--- 09/tarefa09.s
+++ 09/tarefa09.s
@@ -1,5 +1,22 @@
 @ UNICAMP 2018 - MC404 - Lab 09 - Giovanni Bertão - ra173325
 
+.data
+flag_parada:   .word 0
+flag_botao:    .word 0
+paradaX:       .ascii "Centro\n"
+               .byte 0
+paradaY:       .ascii "B. Geraldo\n"
+               .byte 0
+paradaZ:       .ascii "UNICAMP\n"
+               .byte 0
+prox:          .ascii "Proxima Parada:"
+               .byte 0
+atual:         .ascii "Esta e a Parada:"
+               .byte 0
+solici:        .ascii "Parada Solicitada\n"
+               .byte 0
+newline:       .ascii "\n"
+               .byte 0
 .text
 @ Exportar _start como ponto de entrada
 .global _start
@@ -53,10 +70,274 @@ _start:
    msr	cpsr,r0		      @ cpsr: I=0; F=0; MODE=0x10
    mov	sp,#STACK	      @ seta pilha do usuário
 
+   b main                  @ Inicia o programa
+
    @ exit(0)
    mov r0,#0
    mov r7,#1
    svc #0x55
+
+@ Função main
+main:
+   b deixa_Z
+
+@ Chegou na parada
+chega_X:
+
+   bl Limpar_tela          @ Limpa o mostrador
+
+   ldr r1,=atual           @ Escreve mensagem de parada atual
+   bl escreve
+   ldr r1,=paradaX
+   bl escreve
+
+@ Enquanto não deixou a parada X
+loop_parada_X:
+
+   ldr r0,=flag_botao      @ Verifica se deixou a parada
+   ldr r0,[r0]
+   cmp r0,#1               @ Botao de estado=1 -> deixou parada
+   bne loop_parada_X
+
+   ldr r0,=B_PX_partida    @ Verificando qual botão foi pressionado
+   ldr r0,[r0]             @ Seta flag Z
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   cmp r0,#1               
+   bne loop_parada_X       @ Enquanto Z != 1, não chegou na parada
+
+   bl Limpar_tela          @ Limpa o mostrador
+
+@ Deixando a parada
+deixa_X:
+
+   ldr r1,=prox            @ Escreve mensagem de prox parada
+   bl escreve
+   ldr r1,=paradaY
+   bl escreve
+
+   mov r10,#0              @ r10 = 0 -> não existe solicitação de parada
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   mov r7,#0               @ Limpando flag dos botões
+   ldr r6,=flag_parada
+   str r7,[r6]
+   ldr r6,=flag_botao
+   str r7,[r6]
+
+loop_deixa_X:
+                        
+   ldr r0,=flag_parada     @ Verifica se parada foi solicitada
+   ldr r1,[r0]
+   cmp r1,#1
+
+   ldr r1,=paradaY         @ Parada solicitada -> atualizar mostrador
+   bleq Solicitar
+   mov r1,#0
+   str r1,[r0]             @ Reseta botão para 0
+
+   ldr r0,=flag_botao      @ Verificando se chegou na parada de destino
+   ldr r1,[r0]
+   cmp r1,#1
+   bne loop_deixa_X          
+   mov r1,#0
+   str r1,[r0]
+
+   ldr r0,=B_PY_chegada    @ Verifica se o botão certo foi apertado
+   ldr r0,[r0]
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   cmp r0,#1
+   bne loop_deixa_X
+
+@ Chegou na parada
+chega_Y:
+
+   bl Limpar_tela          @ Limpa o mostrador
+
+   ldr r1,=atual           @ Escreve mensagem de parada atual
+   bl escreve
+   ldr r1,=paradaY
+   bl escreve
+
+@ Enquanto não deixou a parada Y
+loop_parada_Y:
+
+   ldr r0,=flag_botao      @ Verifica se deixou a parada
+   ldr r0,[r0]
+   cmp r0,#1               @ Botao de estado=1 -> deixou parada
+   bne loop_parada_Y
+
+   ldr r0,=B_PY_partida    @ Verificando qual botão foi pressionado
+   ldr r0,[r0]             @ Seta flag Z
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   cmp r0,#1               
+   bne loop_parada_Y       @ Enquanto Z != 1, não chegou na parada
+
+   bl Limpar_tela          @ Limpa o mostrador
+
+@ Deixando a parada
+deixa_Y:
+
+   ldr r1,=prox            @ Escreve mensagem de prox parada
+   bl escreve
+   ldr r1,=paradaZ
+   bl escreve
+
+   mov r10,#0              @ r10 = 0 -> não existe solicitação de parada
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   mov r7,#0               @ Limpando flag dos botões
+   ldr r6,=flag_parada
+   str r7,[r6]
+   ldr r6,=flag_botao
+   str r7,[r6]
+
+loop_deixa_Y:
+                        
+   ldr r0,=flag_parada     @ Verifica se parada foi solicitada
+   ldr r1,[r0]
+   cmp r1,#1
+
+   ldr r1,=paradaZ         @ Parada solicitada -> atualizar mostrador
+   bleq Solicitar
+   mov r1,#0
+   str r1,[r0]             @ Reseta botão para 0
+
+   ldr r0,=flag_botao      @ Verificando se chegou na parada de destino
+   ldr r1,[r0]
+   cmp r1,#1
+   bne loop_deixa_Y          
+   mov r1,#0
+   str r1,[r0]
+
+   ldr r0,=B_PZ_chegada    @ Verifica se o botão certo foi apertado
+   ldr r0,[r0]
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   cmp r0,#1
+   bne loop_deixa_Y
+
+@ Chegou na parada
+chega_Z:
+
+   bl Limpar_tela          @ Limpa o mostrador
+
+   ldr r1,=atual           @ Escreve mensagem de parada atual
+   bl escreve
+   ldr r1,=paradaZ
+   bl escreve
+
+@ Enquanto não deixou a parada Z
+loop_parada_Z:
+
+   ldr r0,=flag_botao      @ Verifica se deixou a parada
+   ldr r0,[r0]
+   cmp r0,#1               @ Botao de estado=1 -> deixou parada
+   bne loop_parada_Z
+
+   ldr r0,=B_PZ_partida    @ Verificando qual botão foi pressionado
+   ldr r0,[r0]             @ Seta flag Z
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   cmp r0,#1               
+   bne loop_parada_Z         @ Enquanto Z != 1, não chegou na parada
+
+   bl Limpar_tela          @ Limpa o mostrador
+
+@ Deixando a parada
+deixa_Z:
+
+   ldr r1,=prox            @ Escreve mensagem de prox parada
+   bl escreve
+   ldr r1,=paradaX
+   bl escreve
+
+   mov r10,#0              @ r10 = 0 -> não existe solicitação de parada
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   mov r7,#0               @ Limpando flag dos botões
+   ldr r6,=flag_parada
+   str r7,[r6]
+   ldr r6,=flag_botao
+   str r7,[r6]
+
+loop_deixa_Z:
+                        
+   ldr r0,=flag_parada     @ Verifica se parada foi solicitada
+   ldr r1,[r0]
+   cmp r1,#1
+
+   ldr r1,=paradaX         @ Parada solicitada -> atualizar mostrador
+   bleq Solicitar
+   mov r1,#0
+   str r1,[r0]             @ Reseta botão para 0
+
+   ldr r0,=flag_botao      @ Verificando se chegou na parada de destino
+   ldr r1,[r0]
+   cmp r1,#1
+   bne loop_deixa_Z          
+   mov r1,#0
+   str r1,[r0]
+
+   ldr r0,=B_PX_chegada    @ Verifica se o botão certo foi apertado
+   ldr r0,[r0]
+
+   bl Clear_Buttons        @ Desativa os outros botoes
+
+   cmp r0,#1
+   bne loop_deixa_Z
+
+   b chega_X               @ Reiniciar trajeto
+
+@ Função para Limpar o mostrador
+Limpar_tela:
+   push {lr}               @ Empilha registrador
+   mov r0, #2
+   ldr r1,=newline         @ Escreve \n
+loop_tela:
+   bl escreve
+   subs r0,#1
+   bne loop_tela
+
+   pop {lr}                @ Desempilha
+   bx lr                   @ Retorna
+   
+@ Função para Limpar os botões
+Clear_Buttons:
+   mov r6,#-1
+loop_clear:
+   add r6,#1               @ r1 é o offset
+   ldr r7,=B_parada        @ Endereco inicial do conj de botoes
+   add r7,r6               @ READ( end+offset )
+   ldr r7,[r7]
+   cmp r6,#6               @ Enquanto <6 repete
+   bne loop_clear
+
+   bx lr
+
+@ Função para solicitar parada, recebe prox parada em r1, somente se r10 for 0
+Solicitar:
+   cmp r10,#0
+   moveq r11,r1
+   ldreq r1,=solici        @ Escreve mensagem de prox parada
+   push {lr}               @ Empilha registrador
+   bleq escreve
+   pop {lr}                @ Desempilha
+   moveq r1,r11
+
+   mov r10,#1
+
+   bx lr
 
 @ Função para escrever no console, mensagem passada em r1
 escreve:
@@ -69,34 +350,21 @@ escreve1:
 
    mov r0,#1               @ write(r0, r1, r2) 
    mov r7,#4               @        |   |   |->len
-   svc                     @        |   |->string *
+   svc #0x55               @        |   |->string *
                            @        |->stdout
+
    bx lr                   
 
 @ Tratador IRQ
 tratador_botao:
-   ldb r0,=flag_botao
-   mov r1,#1
-   str r0,[r1]
-   bx lr
+   ldr r8,=flag_botao
+   mov r9,#1
+   str r9,[r8]
+   movs pc,lr
 
 @ Tratador FIQ
 tratador_parada:
-   ldb r8,=flag_botao
+   ldr r8,=flag_parada
    mov r9,#1
    str r9,[r8]
-   bx lr
-
-.data
-paradaX: .ascii "Centro\n"
-         .byte 0
-paradaY: .ascii "B. Geraldo\n"
-         .byte 0
-paradaZ: .ascii "UNICAMP\n"
-         .byte 0
-prox:    .ascii "Proxima Parada:"
-         .byte 0
-atual:   .ascii "Esta e a Parada:"
-         .byte 0
-solici:  .ascii "Parada Solicitada"
-         .byte 0
+   movs pc,lr
